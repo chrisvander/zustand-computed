e41b2c32105da94bfff85705d4b708205d7390d0
mark2685 <mark2685@gmail.com>
2023-06-22 14:50:03 -0700

    fix: Update zustand peerDependencies; Update example zustand. Fix example test

--- src/computed.test.ts
+++ src/computed.test.ts
@@ -1,5 +1,5 @@
-import { computed, ComputedStateOpts } from "./computed"
 import { create } from "zustand"
+import { computed, ComputedStateOpts } from "./computed"
 
 type Store = {
   count: number

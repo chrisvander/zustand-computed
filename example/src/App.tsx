04089ffe37dd2e630343f65ccad64aec532d289c
Christian van der Loo <chris.vanderloo@yahoo.com>
2024-03-03 14:06:21 -0500

    fix: modify example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,13 +2,14 @@ import logo from "./logo.svg"
 import "./App.css"
 import { create } from "zustand"
 import { devtools } from "zustand/middleware"
-import { immer } from "zustand/middleware/immer"
 import { computed } from "zustand-computed"
 
 type Store = {
   count: number
   inc: () => void
   dec: () => void
+  square: () => void
+  root: () => void
 }
 
 type ComputedStore = {
@@ -24,24 +25,33 @@ function computeState(state: Store): ComputedStore {
 const useStore = create<Store>()(
   devtools(
     computed(
-      immer((set) => ({
-        count: 1,
-        inc: () =>
-          set((state) => {
-            // example with Immer middleware
-            state.count += 1
-          }),
-        dec: () => set((state) => ({ count: state.count - 1 })),
-      })),
       computeState,
+      (set, get) =>
+        ({
+          count: 1,
+          inc: () =>
+            set((state) => {
+              // return { countSq: 1 } would error here; SetState does not include ComputedStore
+              return { count: state.count + 1 }
+            }),
+          dec: () => set((state) => ({ count: state.count - 1 })),
+          // the get() function has access to the computed store
+          square: () => set({ count: get().countSq }),
+          root: () => set({ count: Math.floor(Math.sqrt(get().count)) }),
+        }) satisfies Store,
     ),
   ),
 )
 
-useStore.setState({ countSq: 100 }) // error
+// The following line would throw an error as well, for the same reason as above
+//
+// useStore.setState({ countSq: 100 })
+//
+// When using Immer, this line may not error, because Immer creates it's setState function
+// by using the type of the get() function, which includes the ComputedStore
 
 function Counter() {
-  const { count, countSq, inc, dec } = useStore()
+  const { count, countSq, inc, dec, square, root } = useStore()
   return (
     <div className="counter">
       <span>{count}</span>
@@ -53,6 +63,12 @@ function Counter() {
       </button>
       <button type="button" onClick={dec}>
         -1
+      </button>
+      <button type="button" onClick={square}>
+        Square
+      </button>
+      <button type="button" onClick={root}>
+        Root
       </button>
     </div>
   )
